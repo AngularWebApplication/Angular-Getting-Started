5c2129eba5abc1e96ae9c374ea95f5dbac3db65a
Shubhojit Dasgupta <shubhojit.dasgupta@gmail.com>
2019-03-24 11:43:53 +0530

    Generating directives

--- angular-hello/src/app/app.module.ts
+++ angular-hello/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { OrdersComponent } from './orders/orders.component';
+import { SearchBoxDirective } from './search-box.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
-    OrdersComponent
+    OrdersComponent,
+    SearchBoxDirective
   ],
   imports: [
     BrowserModule
